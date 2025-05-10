2fbe713f7bf837c1be5f9fe4eef20a562f666cb2
Thisun-17 <thisun.20233173@iit.ac.lk>
2025-05-10 16:34:06 +0530

    Add Navbar component for navigation

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo">Event Scheduler</Link>
+        <ul className="nav-menu">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/add-event" className="nav-link">Add Event</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
