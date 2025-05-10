3d12e8ccd3c16625e1a9a854d0b653f241222c94
Thisun-17 <thisun.20233173@iit.ac.lk>
2025-05-10 16:41:19 +0530

    Add server.js to set up Express server and MongoDB connection

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const eventRoutes = require('./routes/events');
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+// Routes
+app.use('/api/events', eventRoutes);
+
+// Server setup
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
